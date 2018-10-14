cb305f4c1f9771184344b5062ff338cfd42b3249
wzqq5517992 <wzqq5517992@163.com>
2018-10-14 21:21:38 +0800

    规格开发完成

--- pygCode/pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pygCode/pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -12,8 +12,10 @@ import com.pinyougou.sellergoods.service.BrandService;
 
 import entity.PageResult;
 import entity.Result;
+
 /**
  * 品牌Controller
+ * 
  * @author Jolin
  *
  */
@@ -23,32 +25,37 @@ public class BrandController {
 
 	@Reference
 	private BrandService brandService;
+
 	/**
 	 * 查询所有品牌记录
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/findAll")
-	public List<TbBrand> findAll(){
-		return brandService.findAll();		
+	public List<TbBrand> findAll() {
+		return brandService.findAll();
 	}
-	
+
 	/**
 	 * 品牌分页
+	 * 
 	 * @param page
 	 * @param rows
 	 * @return
 	 */
 	@RequestMapping("/findPage")
-	public PageResult  findPage(int page,int size){			
-		return brandService.findPage(page, size);
+	public PageResult findPage(int page, int rows) {
+		return brandService.findPage(page, rows);
 	}
+
 	/**
 	 * 增加品牌
+	 * 
 	 * @param brand
 	 * @return
 	 */
 	@RequestMapping("/add")
-	public Result add(@RequestBody TbBrand brand){
+	public Result add(@RequestBody TbBrand brand) {
 		try {
 			brandService.add(brand);
 			return new Result(true, "增加成功");
@@ -57,23 +64,26 @@ public class BrandController {
 			return new Result(false, "增加失败");
 		}
 	}
+
 	/**
 	 * 根据ID获取实体
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping("/findOne")
-	public TbBrand findOne(Long id){
-		return brandService.findOne(id);		
+	public TbBrand findOne(Long id) {
+		return brandService.findOne(id);
 	}
 
 	/**
 	 * 修改品牌
+	 * 
 	 * @param brand
 	 * @return
 	 */
 	@RequestMapping("/update")
-	public Result update(@RequestBody TbBrand brand){
+	public Result update(@RequestBody TbBrand brand) {
 		try {
 			brandService.update(brand);
 			return new Result(true, "修改成功");
@@ -82,16 +92,18 @@ public class BrandController {
 			return new Result(false, "修改失败");
 		}
 	}
+
 	/**
 	 * 批量删除
+	 * 
 	 * @param ids
 	 * @return
 	 */
 	@RequestMapping("/delete")
-	public Result delete(Long [] ids){
+	public Result delete(Long[] ids) {
 		try {
 			brandService.delete(ids);
-			return new Result(true, "删除成功"); 
+			return new Result(true, "删除成功");
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new Result(false, "删除失败");
@@ -100,18 +112,15 @@ public class BrandController {
 
 	/**
 	 * 查询+分页
+	 * 
 	 * @param brand
 	 * @param page
 	 * @param rows
 	 * @return
 	 */
 	@RequestMapping("/search")
-	public PageResult search(@RequestBody TbBrand brand, int page, int rows  ){
-		return brandService.findPage(brand, page, rows);		
+	public PageResult search(@RequestBody TbBrand brand, int page, int rows) {
+		return brandService.findPage(brand, page, rows);
 	}
 
-
-
-
-	
 }
