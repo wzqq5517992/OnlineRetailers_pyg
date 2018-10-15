465086d4e264b09bf3167acd9ab442b0e62d796c
wzqq5517992 <wzqq5517992@163.com>
2018-10-15 23:32:33 +0800

    模板管理完成

--- pygCode/pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SpecificationServiceImpl.java
+++ pygCode/pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SpecificationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.pinyougou.sellergoods.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -145,6 +146,12 @@ public class SpecificationServiceImpl implements SpecificationService {
 
 		Page<TbSpecification> page = (Page<TbSpecification>) specificationMapper.selectByExample(example);
 		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	@Override
+	public List<Map> selectOptionList() {
+		// TODO Auto-generated method stub
+		return specificationMapper.selectOptionList();
 	}
 
 }
