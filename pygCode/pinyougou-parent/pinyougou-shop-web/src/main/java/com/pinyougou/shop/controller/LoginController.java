54c5f9974b9198511443b2b406c8c0601dfd9d51
wzqq5517992 <wzqq5517992@163.com>
2018-10-23 22:41:15 +0800

    商品录入富文本开发完成

---
+++ pygCode/pinyougou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.pinyougou.shop.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * 
+ * 显示登录名
+ * @author Jolin
+ *
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+	@RequestMapping("/name")
+	public Map name(){
+		String name=SecurityContextHolder.getContext().getAuthentication().getName();
+		System.out.println(name);
+		Map map=new HashMap();
+		map.put("loginName", name);
+
+		return map;
+		
+	}
+}
