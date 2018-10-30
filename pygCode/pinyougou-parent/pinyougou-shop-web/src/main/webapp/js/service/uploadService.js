5ad4ec2e959f1cfaf5c23fe117fa48230673d482
wzq <weizhiqiang@infohold.com.cn>
2018-10-30 08:14:38 +0800

    123

--- pygCode/pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/service/uploadService.js
+++ pygCode/pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/service/uploadService.js
@@ -1,19 +1,14 @@
-app.service('uploadService',function($http){
-	
-	//上传文件
+//文件上传服务层
+app.service("uploadService",function($http){
 	this.uploadFile=function(){
-		var formdata=new FormData();
-		formdata.append('file',file.files[0]);//file 文件上传框的name
-		
+		var formData=new FormData();
+	    formData.append("file",file.files[0]);   
 		return $http({
-			url:'../upload.do',		
-			method:'post',
-			data:formdata,
-			headers:{ 'Content-Type':undefined },
-			transformRequest: angular.identity			
-		});
-		
-	}
-	
-	
+            method:'POST',
+            url:"../upload.do",
+            data: formData,
+            headers: {'Content-Type':undefined},//anjularjs对于post和get请求默认的Content-Type header 是application/json。通过设置‘Content-Type’: undefined，这样浏览器会帮我们把Content-Type 设置为 multipart/form-data.
+            transformRequest: angular.identity //对表单进行序列化
+        });		
+	}	
 });
