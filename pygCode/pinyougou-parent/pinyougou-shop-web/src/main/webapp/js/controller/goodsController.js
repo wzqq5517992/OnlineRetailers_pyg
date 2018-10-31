59fa51ce0b81eeee264e44a17513dc27e80da693
wzqq5517992 <wzqq5517992@163.com>
2018-10-31 23:13:03 +0800

    7-2-5

--- pygCode/pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pygCode/pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('goodsController' ,function($scope,$controller   ,goodsService,uploadService,itemCatService,typeTemplateService){	
+app.controller('goodsController' ,function($scope,$controller,$location,goodsService,uploadService,itemCatService,typeTemplateService){	
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -23,10 +23,27 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 	}
 	
 	//查询实体 
-	$scope.findOne=function(id){				
+	$scope.findOne=function(id){
+		var id= $location.search()['id'];//获取参数值
+		if(id==null){
+			return ;
+		}
 		goodsService.findOne(id).success(
 			function(response){
-				$scope.entity= response;					
+				$scope.entity= response;
+				//向富文本编辑器添加商品介绍
+				editor.html($scope.entity.goodsDesc.introduction);
+				 //显示图片列表
+				$scope.entity.goodsDesc.itemImages=  
+				JSON.parse($scope.entity.goodsDesc.itemImages);
+				
+				//显示扩展属性
+				$scope.entity.goodsDesc.customAttributeItems=JSON.parse($scope.entity.goodsDesc.customAttributeItems);
+				//规格	
+				$scope.entity.goodsDesc.specificationItems=JSON.parse($scope.entity.goodsDesc.specificationItems);
+
+
+
 			}
 		);				
 	}
@@ -173,8 +190,11 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 				$scope.typeTemplate=response;// 模板对象 
 								
 				$scope.typeTemplate.brandIds= JSON.parse($scope.typeTemplate.brandIds);//品牌列表类型转换
-				//扩展属性
-				$scope.entity.goodsDesc.customAttributeItems= JSON.parse($scope.typeTemplate.customAttributeItems);
+				//如果没有ID，则加载模板中的扩展数据
+				if($location.search()['id']==null){//如果是增加商品
+					$scope.entity.goodsDesc.customAttributeItems = JSON.parse($scope.typeTemplate.customAttributeItems);//扩展属性	
+				}
+
 			}
 		);
 		//读取规格
@@ -250,6 +270,23 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 				}
 		);
 	}
+	
+	//根据规格名称和选项名称返回是否被勾选
+	$scope.checkAttributeValue=function(specName,optionName){//能够查到
+		var items= $scope.entity.goodsDesc.specificationItems;
+		var object= $scope.searchObjectByKey(items,'attributeName',specName);
+		if(object==null){
+			return false;
+		}else{
+			if(object.attributeValue.indexOf(optionName)>=0){
+				return true;
+			}else{
+				return false;
+			}
+		}			
+	}
+
+	
 
 	
 	
