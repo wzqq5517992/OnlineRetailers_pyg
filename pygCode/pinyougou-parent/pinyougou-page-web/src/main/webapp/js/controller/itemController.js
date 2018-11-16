b5fb879a889dbbf011954b9ea30b9c5f3894c5d4
wzqq5517992 <wzqq5517992@163.com>
2018-11-16 20:51:13 +0800

    12end

---
+++ pygCode/pinyougou-parent/pinyougou-page-web/src/main/webapp/js/controller/itemController.js
@@ -0,0 +1,10 @@
+//商品详细页（控制层）
+app.controller('itemController',function($scope){
+	//数量操作
+	$scope.addNum=function(x){
+		$scope.num=$scope.num+x;
+		if($scope.num<1){
+			$scope.num=1;
+		}
+	}		
+});
